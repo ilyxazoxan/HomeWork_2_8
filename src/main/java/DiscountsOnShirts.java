ed7f8f7ca41258b863f8baf4ff78f1a29799129b
Ilya Danilov <esozap32@mail.ru>
2022-10-31 00:06:34 +0400

    DiscountsOnShirts

--- src/main/java/DiscountsOnShirts.java
+++ src/main/java/DiscountsOnShirts.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class DiscountsOnShirts {
+public class DiscountsOnShirts {
+    public static void main(String[] args) {
+        int shirtPrice = 1350;
+          System.out.println("Цена рубашки: " + shirtPrice + (" Рублей"));
+
+        int selectedShirts = 8;
+          System.out.println("Рубашек выбрано: " + selectedShirts + (" Штук"));
+
+        int discountFrom = 10;
+          System.out.println("За сколько рубашек идет скидка: " + discountFrom + (" Штук"));
+
+        long add = discountFrom - selectedShirts;
+          System.out.println("Столько рубашек нужно докупить для получения скидки: " + add + (" Штук"));
+
+        long price = selectedShirts * shirtPrice;
+          System.out.println("Цена выбранных рубашек без скидки: " + price + (" Рублей"));
+
+        long priceAll = discountFrom * shirtPrice;
+          System.out.println("Цена за количество рубашек, от которого количества идет скидка: " + priceAll + (" Рублей"));
+
+        double discount = 0.38;
+          System.out.println("Процент скидки при покупке 10 рубашек: " + discount + (" Процентов"));
+
+        double discountedPriceOfAllShirts = priceAll * discount;
+          System.out.println("Процент скидки от всех рубашек " + discountedPriceOfAllShirts + (" Рублей"));
+
+        double discountedPriceOfOneShirt = shirtPrice - (shirtPrice * discount);
+          System.out.println("Цена одной рубашки со скидкой " + discountedPriceOfOneShirt + (" Рублей"));
+
+        double saved = (priceAll - (discountedPriceOfOneShirt * discountFrom));
+          System.out.println("Сумма сэкономленных денег " + saved);
+
+        double freebie = saved / shirtPrice;
+          System.out.println("Количество рубашек за сэкономленные деньги " + freebie + (" Штук"));
+    }
 }
